f755ad9d659b62c599921f3509fbe8f8152b5134
arturaronov <unionco@icloud.com>
2023-10-17 14:39:28 +0800

    changes

--- src/LogAnalyzer.java
+++ src/LogAnalyzer.java
@@ -1,9 +1,23 @@
 import edu.duke.FileResource;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LogAnalyzer {
     private static ArrayList<LogEntry> records;
+    public HashMap<String, Integer> countVisitsPerIp(){
+        HashMap<String, Integer> counts = new HashMap<String, Integer>();
+        for(LogEntry le: records) {
+            String ip = le.getIpAddress();
+            if(!counts.containsKey(ip)) {
+                counts.put(ip, 1);
+            } else {
+                counts.put(ip, counts.get(ip) + 1);
+            }
+        }
+
+        return counts;
+    }
 
     public LogAnalyzer() {
         records = new ArrayList<LogEntry>();
@@ -31,5 +45,28 @@ public class LogAnalyzer {
         }
 
         return uniqueIPs.size();
+    }
+
+    public int countTotalVisits() {
+        int total = 0;
+        for(LogEntry le: records){
+            total++;
+        }
+
+        return total;
+    }
+
+    public Integer iPRanges() {
+        int total = 0;
+        for(LogEntry le: records) {
+            String ipAddr = le.getIpAddress();
+            String[] ipAddrSplit = ipAddr.split("[.]");
+            Integer iPNum = Integer.parseInt(ipAddrSplit[0]);
+            if(iPNum > 199 && iPNum < 300) {
+                total++;
+            }
+        }
+
+        return total;
     }
 }
